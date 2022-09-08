18068df7a53bf21d22454fec0c80c64f45b494f8
JongWooBAE <whddnbae@gmail.com>
2022-09-08 11:22:03 +0900

    aws-sdk로 변경

--- pages/api/s3-upload.ts
+++
@@ -1,9 +0,0 @@
-import { APIRoute } from "next-s3-upload";
-
-
-export default APIRoute.configure({
-    key(req, filename){
-        let id = req.body.id
-        return `${id}/${filename}`;
-    }
-});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,55 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import UploadImages from '../components/upload'
+import { useState } from "react";
+import axios from "axios";
 
-const Home: NextPage = () => {
+// const BUCKET_URL = "https://testyhyrasz1312.s3.eu-central-1.amazonaws.com/";
+
+export default function Home() {
+  const [file, setFile] = useState<any>();
+  const [uploadingStatus, setUploadingStatus] = useState<any>();
+
+  const selectFile = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const uploadFile = async () => {
+    setUploadingStatus("Uploading the file to AWS S3");
+
+    let { data } = await axios.post("/api/s3/upload", {
+      name: file.name,
+      type: file.type,
+    });
+
+    console.log(data);
+
+    const url = data.url;
+    let { data: newData } = await axios.put(url, file, {
+      headers: {
+        "Content-type": file.type,
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+
+    setFile(null);
+  };
+
   return (
     <div>
-      <UploadImages />
+      <main>
+        <p>Please select a file to upload</p>
+        <input type="file" onChange={(e) => selectFile(e)} />
+        {file && (
+          <>
+            <p>Selected file: {file.name}</p>
+            <button
+              onClick={uploadFile}
+              className=" bg-purple-500 text-white p-2 rounded-sm shadow-md hover:bg-purple-700 transition-all"
+            >
+              Upload a File!
+            </button>
+          </>
+        )}
+        {uploadingStatus && <p>{uploadingStatus}</p>}
+      </main>
     </div>
-  )
+  );
 }
-
-export default Home
